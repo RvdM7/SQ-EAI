0f3bcd936942c051890172afb07fdfd3878dc928
Masaru <rvdm1152@gmail.com>
2021-01-05 21:06:41 +0100

    Update IBericht.java

--- src/main/java/hanze/nl/model/IBericht.java
+++ src/main/java/hanze/nl/model/IBericht.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 
 public interface IBericht {
 
-//    public static AbstractBericht newBericht(String lijnNaam, String bedrijf, String busID, int tijd){
-//        return new Bericht(lijnNaam, bedrijf, busID, tijd);
-//    }
-
     public String getEindpunt();
     public void setEindpunt(String newEindpunt);
     public ArrayList<IETA> getETAs();
