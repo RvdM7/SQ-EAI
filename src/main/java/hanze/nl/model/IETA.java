51df7ec3aac2066d677e04d6e352544b80d65fd5
Masaru <rvdm1152@gmail.com>
2021-01-05 21:06:56 +0100

    Update IETA.java

--- src/main/java/hanze/nl/model/IETA.java
+++ src/main/java/hanze/nl/model/IETA.java
@@ -2,7 +2,4 @@ package hanze.nl.model;
 
 public interface IETA {
 
-//    public static AbstractETA createETA(String halteNaam, int richting, int aankomsttijd){
-//        return new ETA(halteNaam, richting, aankomsttijd);
-//    }
 }
